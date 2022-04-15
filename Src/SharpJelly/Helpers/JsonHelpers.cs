fd538e2a0e333c8e437f0daf804cbb1a788efe10
Jerhynh <60614943+Jerhynh@users.noreply.github.com>
2022-04-14 23:47:54 -0500

    Implement Json Helpers

---
+++ Src/SharpJelly/Helpers/JsonHelpers.cs
@@ -0,0 +1,29 @@
+﻿using SharpJelly.Models;
+using System.Text.Json;
+
+namespace SharpJelly.Helpers
+{
+    public class JsonHelpers
+    {
+        public static JFUser? FindJFUser(string payload, string username)
+        {
+            var JsonObject = JsonDocument.Parse(payload);
+            foreach (var array in JsonObject.RootElement.EnumerateArray())// loop each object contained in root array.
+            {
+                foreach (var obj in array.EnumerateObject())// loop each element contained within the object in context.
+                {
+                    if (obj.Name == "Name")
+                    {
+                        if (obj.Value.GetString() == username)
+                        {
+                            return JsonSerializer.Deserialize<JFUser>(array);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+
+    }
+}
